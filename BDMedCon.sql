9c87a711b077e2af2b0ad68cee486bcd19bbf563
Patriciatr <patriciatr99@uma.es>
2021-02-05 23:54:42 +0100

    Base de Datos MedCon

--- BDMedCon.sql
+++ BDMedCon.sql
@@ -1,121 +1,138 @@
-USE [medCon]
-GO
-/****** Object:  Table [dbo].[medico]    Script Date: 05/02/2021 22:19:58 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[medico](
-	[medicoID] [int] IDENTITY(1,1) NOT NULL,
-	[DNI] [varchar](13) NULL,
-	[Password] [varchar](255) NULL,
-	[Nombre] [varchar](255) NULL,
-	[Apellidos] [varchar](255) NULL,
-	[medicoJefe] [varchar](255) NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[medicoID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[paciente]    Script Date: 05/02/2021 22:19:59 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[paciente](
-	[pacienteID] [int] IDENTITY(1,1) NOT NULL,
-	[DNI] [varchar](13) NULL,
-	[Password] [varchar](255) NULL,
-	[Nombre] [varchar](255) NULL,
-	[Apellidos] [varchar](255) NULL,
-	[NumSS] [varchar](13) NULL,
-	[Sexo] [varchar](255) NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[pacienteID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-SET IDENTITY_INSERT [dbo].[medico] ON 
-GO
-INSERT [dbo].[medico] ([medicoID], [DNI], [Password], [Nombre], [Apellidos], [medicoJefe]) VALUES (1, N'16040618D', N'QBB00OYR6WU', N'Pedro', N'Cortez', N'0')
-GO
-INSERT [dbo].[medico] ([medicoID], [DNI], [Password], [Nombre], [Apellidos], [medicoJefe]) VALUES (2, N'16820120G', N'CWV65NQL4MN', N'Lucia', N'Garrison', N'0')
-GO
-INSERT [dbo].[medico] ([medicoID], [DNI], [Password], [Nombre], [Apellidos], [medicoJefe]) VALUES (3, N'16440603H', N'IDJ87OKV1CK', N'Isadora', N'Robinson', N'1')
-GO
-INSERT [dbo].[medico] ([medicoID], [DNI], [Password], [Nombre], [Apellidos], [medicoJefe]) VALUES (4, N'16370510K', N'FNX45JDV9RE', N'Maia', N'Anderson', N'0')
-GO
-SET IDENTITY_INSERT [dbo].[medico] OFF
-GO
-SET IDENTITY_INSERT [dbo].[paciente] ON 
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (1, N'16541029Q', N'DQE22GUC2TA', N'Brittany', N'Barnes', N'169609179214', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (2, N'16280120W', N'SLN81MJH8LP', N'Roman', N'Perez', N'165801229187', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (3, N'16690206E', N'VHP92GEC7HG', N'Maria', N'Gonzales', N'161909083857', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (4, N'16730807R', N'OXV90QGB6MS', N'Lorena', N'Jensen', N'163405128053', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (5, N'16250403T', N'IQU02MFG8UI', N'Manuel', N'Delacruz', N'166707245020', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (6, N'16241211Y', N'XZI93FAJ2QX', N'Clara', N'Vargas', N'169610139215', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (7, N'16321002U', N'FJM60PHZ4JL', N'Ana', N'Pittman', N'162104188459', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (8, N'16210308I', N'JSL96BKJ9PY', N'Matilda', N'Walton', N'165703122696', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (9, N'16320930O', N'JJN26HZN6DN', N'Kiara', N'Martin', N'162804306625', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (10, N'16360120P', N'ZFC60MTE3CF', N'Darrel', N'Larson', N'168705024233', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (11, N'16000916A', N'AAZ51GTN8YV', N'Antonio', N'Hopkins', N'162010104772', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (12, N'16131206S', N'OBM99EBS6WN', N'Pedro', N'Kent', N'163810223085', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (13, N'16570422D', N'OWJ52WXL6MH', N'Tad', N'Ochoa', N'160208292946', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (14, N'16260403F', N'OQL04TQN0EE', N'Daniel', N'Garcia', N'164402205530', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (15, N'16471212G', N'BPK87FPC6QS', N'Summer', N'Mann', N'169911052554', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (16, N'16880630H', N'KMV36HIO1QK', N'Lucia', N'Frazier', N'163107050498', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (17, N'16480712J', N'INH45BTW6QW', N'Daniel', N'Glass', N'169409171999', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (18, N'16100301K', N'QNG07TYR2UN', N'Carolina', N'Lee', N'162909156180', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (19, N'16950101L', N'VRW31CVS6RG', N'Jackson', N'Small', N'164410140117', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (20, N'16700830Z', N'FLC41LIL1BS', N'Victoria', N'Reilly', N'164708245198', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (21, N'16580529X', N'PXX88AHX6YF', N'Airan', N'Valverde', N'167909037256', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (22, N'16020721C', N'FLZ48WFX1AV', N'Neville', N'Oneal', N'165606033180', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (23, N'16780709V', N'JJY85DUX7SQ', N'Kenia', N'Nielsen', N'165405196568', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (24, N'16721201B', N'JLQ72ZWZ5GA', N'Andrea', N'Mendez', N'164603192172', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (25, N'16690928N', N'COT76ZXR1IO', N'Taylor', N'Swift', N'160909110983', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (26, N'16430830M', N'MUG05BKI6XN', N'Ronaldo', N'Todd', N'169407097626', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (27, N'16120727Q', N'RVO45NIS9JT', N'Mao', N'Lang', N'165703298124', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (28, N'16521226E', N'NAF46AHV5FC', N'Ines', N'Whitehead', N'165707266689', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (29, N'16601022R', N'LHW41ZNV2LK', N'Calvin', N'Coleman', N'168503242995', N'Hombre')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (30, N'16520527T', N'BXM55GFW2RI', N'Eva', N'Gibbs', N'168707280916', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (31, N'16640714Y', N'SND40PSW6LG', N'Elena', N'Campos', N'160906068986', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (32, N'16871121U', N'LWC72WMJ0NZ', N'Mara', N'Coffey', N'167309193790', N'Mujer')
-GO
-INSERT [dbo].[paciente] ([pacienteID], [DNI], [Password], [Nombre], [Apellidos], [NumSS], [Sexo]) VALUES (33, N'16881007D', N'VWY25OWC5BK', N'Lara', N'Giles', N'162912262876', N'Mujer')
-GO
-SET IDENTITY_INSERT [dbo].[paciente] OFF
-GO
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-02-2021 a las 23:53:40
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bdmedcon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medico`
+--
+
+CREATE TABLE `medico` (
+  `id` int(11) NOT NULL,
+  `DNI` varchar(13) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `Nombre` varchar(255) DEFAULT NULL,
+  `Apellidos` varchar(255) DEFAULT NULL,
+  `medicoJefe` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `medico`
+--
+
+INSERT INTO `medico` (`id`, `DNI`, `Password`, `Nombre`, `Apellidos`, `medicoJefe`) VALUES
+(1, '16040618D', 'QBB00OYR6WU', 'Pedro', 'Cortez', '0'),
+(2, '16820120G', 'CWV65NQL4MN', 'Lucia', 'Garrison', '0'),
+(3, '16440603H', 'IDJ87OKV1CK', 'Isadora', 'Robinson', '1'),
+(4, '16370510K', 'FNX45JDV9RE', 'Maia', 'Anderson', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `id` int(11) NOT NULL,
+  `DNI` varchar(13) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `Nombre` varchar(255) DEFAULT NULL,
+  `Apellidos` varchar(255) DEFAULT NULL,
+  `NumSS` varchar(13) DEFAULT NULL,
+  `Sexo` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `paciente`
+--
+
+INSERT INTO `paciente` (`id`, `DNI`, `Password`, `Nombre`, `Apellidos`, `NumSS`, `Sexo`) VALUES
+(1, '16541029Q', 'DQE22GUC2TA', 'Brittany', 'Barnes', '169609179214', 'Mujer'),
+(2, '16280120W', 'SLN81MJH8LP', 'Roma', 'Perez', '165801229187', 'Hombre'),
+(3, '16690206E', 'VHP92GEC7HG', 'Maria', 'nzales', '161909083857', 'Mujer'),
+(4, '16730807R', 'OXV90QGB6MS', 'Lorena', 'Jense', '163405128053', 'Mujer'),
+(5, '16250403T', 'IQU02MFG8UI', 'Manuel', 'Delacruz', '166707245020', 'Hombre'),
+(6, '16241211Y', 'XZI93FAJ2QX', 'Clara', 'Vargas', '169610139215', 'Mujer'),
+(7, '16321002U', 'FJM60PHZ4JL', 'Ana', 'Pittma', '162104188459', 'Mujer'),
+(8, '16210308I', 'JSL96BKJ9PY', 'Matilda', 'Walto', '165703122696', 'Mujer'),
+(9, '16320930O', 'JJN26HZN6D', 'Kiara', 'Marti', '162804306625', 'Mujer'),
+(10, '16360120P', 'ZFC60MTE3CF', 'Darrel', 'Larso', '168705024233', 'Mujer'),
+(11, '16000916A', 'AAZ51GTN8YV', 'Antonio', 'Hopkins', '162010104772', 'Hombre'),
+(12, '16131206S', 'OBM99EBS6W', 'Pedro', 'Kent', '163810223085', 'Hombre'),
+(13, '16570422D', 'OWJ52WXL6MH', 'Tad', 'Ochoa', '160208292946', 'Hombre'),
+(14, '16260403F', 'OQL04TQN0EE', 'Daniel', 'Garcia', '164402205530', 'Hombre'),
+(15, '16471212G', 'BPK87FPC6QS', 'Summer', 'Ma', '169911052554', 'Mujer'),
+(16, '16880630H', 'KMV36HIO1QK', 'Lucia', 'Frazier', '163107050498', 'Mujer'),
+(17, '16480712J', 'INH45BTW6QW', 'Daniel', 'Glass', '169409171999', 'Hombre'),
+(18, '16100301K', 'QNG07TYR2U', 'Carolina', 'Lee', '162909156180', 'Mujer'),
+(19, '16950101L', 'VRW31CVS6RG', 'Jackso', 'Small', '164410140117', 'Hombre'),
+(20, '16700830Z', 'FLC41LIL1BS', 'Victoria', 'Reilly', '164708245198', 'Mujer'),
+(21, '16580529X', 'PXX88AHX6YF', 'Aira', 'Valverde', '167909037256', 'Hombre'),
+(22, '16020721C', 'FLZ48WFX1AV', 'Neville', 'Oneal', '165606033180', 'Hombre'),
+(23, '16780709V', 'JJY85DUX7SQ', 'Kenia', 'Nielse', '165405196568', 'Mujer'),
+(24, '16721201B', 'JLQ72ZWZ5GA', 'Andrea', 'Mendez', '164603192172', 'Mujer'),
+(25, '16690928', 'COT76ZXR1IO', 'Taylor', 'Swift', '160909110983', 'Mujer'),
+(26, '16430830M', 'MUG05BKI6X', 'Ronaldo', 'Todd', '169407097626', 'Hombre'),
+(27, '16120727Q', 'RVO45NIS9JT', 'Mao', 'Lang', '165703298124', 'Hombre'),
+(28, '16521226E', 'NAF46AHV5FC', 'Ines', 'Whitehead', '165707266689', 'Mujer'),
+(29, '16601022R', 'LHW41ZNV2LK', 'Calvi', 'Colema', '168503242995', 'Hombre'),
+(30, '16520527T', 'BXM55GFW2RI', 'Eva', 'Gibbs', '168707280916', 'Mujer'),
+(31, '16640714Y', 'SND40PSW6LG', 'Elena', 'Campos', '160906068986', 'Mujer'),
+(32, '16871121U', 'LWC72WMJ0NZ', 'Mara', 'Coffey', '167309193790', 'Mujer');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `medico`
+--
+ALTER TABLE `medico`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `medico`
+--
+ALTER TABLE `medico`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
