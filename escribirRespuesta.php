fa2602e86a5e591c2c3b62119e9dd7196c8c1ccf
LuciaVM <lu.valverdemartinez@gmail.com>
2021-02-09 20:33:28 +0100

    Cambios de estilo, cambio en h1, h2... y en escribirRespuesta

--- escribirRespuesta.php
+++ escribirRespuesta.php
@@ -18,26 +18,26 @@
     <body>
         <div class="gwd-div-lm07"></div>
         <img src="assets/logo.png" class="gwd-img-fa6j">
+        <h4 class = "area" >Área de médico</h4>
         <nav id="menu-superior">
             <ul>
-                <li><a href="listaConsultasMedico.php?idMed=<?php echo $idMed?>"><h3 class="gwd-p-gv4z" id="listConsultas">Consultas Activas</h3></a></li>
-                <li class="gwd-li-yj6f"><a href="listaPacientesMedico.php?idMed=<?php echo $idMed?>"><h3 class="gwd-p-gv4z gwd-p-1qhn" id="fichaPaciente">Pacientes</h3></a></li>
-                <li><a href="login.php"><h3 class="gwd-p-gv4z gwd-p-5vs1 " id="salir">Salir</h3></a></li>
+                <li class="gwd-p-gv4z"><a href="listaConsultasMedico.php?idMed=<?php echo $idMed?>">Consultas Activas</a></li>
+                <li class="gwd-p-gv4z gwd-li-yj6f"><a href="listaPacientesMedico.php?idMed=<?php echo $idMed?>">Pacientes</a></li>
+                <li class="gwd-p-gv4z gwd-p-5vs1"><a href="login.php">Salir</a></li>
             </ul>
         </nav>
-        <div class="form">
+
+
+        <div class="form2">
+            <h1> Formulario escribir una respuesta </h1>
+            <br>  </br>
             <form name="formulario" method="POST" enctype="multipart/form-data">
                 <table id="tablaRespuesta">
-                    <tr><td><pre><b>Texto   </b></pre></td>
+                    <tr><td><pre><b>Texto </b></pre></td>
                         <td><textarea id="textoRespuesta" name="textoRespuesta" rows="4" cols="50" autocapitalize="sentences" placeholder="Escribe tu respuesta"></textarea></td>
                     </tr>
                     <tr><td><pre><b>Imagen o Archivo   </b></pre></td>
-                        <td><input type="file" name="archivo" id="archivo"></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-                            <input type="checkbox" name="respondida">La consulta ha sido respondida por completo.
-                        </td>
+                        <td><input class = "botonEscribirRespuesta" type="file" name="archivo" id="archivo"></td>
                     </tr>
                     <tr>
                         <td colspan="2">
@@ -45,46 +45,27 @@
                         </td>
                     </tr>
                     <tr>
-                        <td colspan="2">
-                            <input type="submit" name="enviar">
-                            <input type="reset" name="borrar">
+                        <td id = "filaBotonesRespuesta" colspan="2">
+                            <input class = "botonEscribirRespuesta" type="submit" name="enviar">
+                            <input type="reset" class = "botonEscribirRespuesta" name="borrar">
                         </td>
                     </tr>
                 </table>
                 <?php
             if(isset($_REQUEST['enviar'])):
-                $archSubir = NULL;
-                if (!empty($_FILES["archivo"]["name"])){
-                    $target_dir = "uploads/";
-                    $archSubir = $target_dir . "arch-Consulta". $IDconsulta . "-" .  basename($_FILES["archivo"]["name"]);
-                    $uploadOk = 1;
+                $target_dir = "uploads/";
+                $archSubir = $target_dir . "arch-Consulta". $IDconsulta . "-" .  basename($_FILES["archivo"]["name"]);
+                $uploadOk = 1;
 
-                    // Check if $uploadOk is set to 0 by an error
-                    if ($uploadOk == 0) {
-                        echo "Sorry, your file was not uploaded.";
+                // Check if $uploadOk is set to 0 by an error
+                if ($uploadOk == 0) {
+                    echo "Sorry, your file was not uploaded.";
 
-                    // if everything is ok, try to upload file
-                    } else {
-                        move_uploaded_file($_FILES["archivo"]["tmp_name"], $archSubir);
-                    }
+                // if everything is ok, try to upload file
+                } else {
+                    move_uploaded_file($_FILES["archivo"]["tmp_name"], $archSubir);
                 }
-
-                // $consultaBorrar = $miPDO -> prepare('UPDATE paciente SET Medico = -1 WHERE DNI = :dni');
-                // $ok = $consultaBorrar -> execute(array('dni' => $dniPacBorrar));
-
-                if(isset($_POST['respondida'])){
-                    if($IDconsulta[0]=="c"){
-                        $actualizar = $miPDO -> prepare('UPDATE `consultacovid` SET `respondida`=1 WHERE `ID` LIKE :IDconsulta');
-                        $ok = $actualizar -> execute(array('IDconsulta' => $IDconsulta)); 
-                    } else if($IDconsulta[0]=="o"){
-                        $actualizar = $miPDO -> prepare('UPDATE `consultaotra` SET `respondida`=1 WHERE `ID` LIKE :IDconsulta');
-                        $ok = $actualizar -> execute(array('IDconsulta' => $IDconsulta));
-                    } else if($IDconsulta[0]=="p"){
-                        $actualizar = $miPDO -> prepare('UPDATE `consultaperiodica` SET `respondida`=1 WHERE `ID` LIKE :IDconsulta');
-                        $ok = $actualizar -> execute(array('IDconsulta' => $IDconsulta)); 
-                    }
-                }
-
+                
                 $texto= isset($_POST['textoRespuesta']) ? $_POST['textoRespuesta'] : null;
                 $archivo= $archSubir;
                 $fecha = date('Y-m-d');
@@ -100,7 +81,8 @@
                     ));
                     $miPDO -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
                     
-                    if($ok) echo 'Insertado correctamente';                                          
+                    if($ok) echo 'Insertado correctamente';
+                                          
             endif ?>
             </form>
         </div>
